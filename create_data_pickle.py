668bac6e4b19fb879e590475a6d81c50d5669219
Piotr Kozakowski <kozak000@gmail.com>
2018-08-07 15:48:16 +0200

    Switch backend to TensorFlow

--- create_data_pickle.py
+++ create_data_pickle.py
@@ -2,7 +2,7 @@ from common import load_track, GENRES
 import sys
 import numpy as np
 from math import pi
-from cPickle import dump
+from pickle import dump
 import os
 from optparse import OptionParser
 
@@ -30,10 +30,10 @@ def collect_data(dataset_path):
     track_paths = {}
 
     for (genre_index, genre_name) in enumerate(GENRES):
-        for i in xrange(TRACK_COUNT // len(GENRES)):
+        for i in range(TRACK_COUNT // len(GENRES)):
             file_name = '{}/{}.000{}.au'.format(genre_name,
                     genre_name, str(i).zfill(2))
-            print 'Processing', file_name
+            print('Processing', file_name)
             path = os.path.join(dataset_path, file_name)
             track_index = genre_index  * (TRACK_COUNT // len(GENRES)) + i
             x[track_index], _ = load_track(path, default_shape)
@@ -55,5 +55,5 @@ if __name__ == '__main__':
     (x, y, track_paths) = collect_data(options.dataset_path)
 
     data = {'x': x, 'y': y, 'track_paths': track_paths}
-    with open(options.output_pkl_path, 'w') as f:
+    with open(options.output_pkl_path, 'wb') as f:
         dump(data, f)
--- genre_recognizer.py
+++ genre_recognizer.py
@@ -1,8 +1,8 @@
 from common import load_track, get_layer_output_function
 import numpy as np
-from keras.layers import Input
-from keras.models import model_from_yaml, Model
-from keras import backend as K
+from tensorflow.keras.layers import Input
+from tensorflow.keras.models import model_from_yaml, Model
+from tensorflow.keras import backend as K
 
 class GenreRecognizer():
 
@@ -11,10 +11,10 @@ class GenreRecognizer():
             model = model_from_yaml(f.read())
         model.load_weights(weights_path)
         self.pred_fun = get_layer_output_function(model, 'output_realtime')
-        print 'Loaded model.'
+        print('Loaded model.')
     
     def recognize(self, track_path):
-        print 'Loading song', track_path
+        print('Loading song', track_path)
         (features, duration) = load_track(track_path)
         features = np.reshape(features, (1,) + features.shape)
         return (self.pred_fun(features), duration)
